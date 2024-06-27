f12d126b3b0adbd2ba391c269c7cb9d051db66de
Ashique <ashiqurrahman2092003@gmail.com>
2024-06-27 22:04:45 +0600

    remove-comment-2

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+  
 
     const artCollection = client.db('artDb').collection('art')
     const categoryCollection = client.db('artDb').collection('categoryCollection')
